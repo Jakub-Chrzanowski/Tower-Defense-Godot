e6659b45a6230bf9d5e4e7e9ce105126185144bb
s-swiatek <s.swiatek.tp22@zsme.tarnow.pl>
2026-04-26 18:36:20 +0100

    Update SceneNav.cs
    musztarda i do pica

--- Scripts/SceneNav.cs
+++ Scripts/SceneNav.cs
@@ -2,10 +2,10 @@ using Godot;
 
 public static class ScenePaths
 {
-    public const string MainMenu = "res://Scenes/MainMenu.tscn";
-    public const string Options = "res://Scenes/Options.tscn";
+    public const string MainMenu   = "res://Scenes/MainMenu.tscn";
+    public const string Options    = "res://Scenes/Options.tscn";
     public const string LevelSelect = "res://Scenes/LevelSelect.tscn";
-    public const string Game = "res://Scenes/Game.tscn";
+    public const string Game       = "res://Scenes/Game.tscn";
 }
 
 public static class SceneNav
@@ -15,5 +15,30 @@ public static class SceneNav
 
 public static class GameSession
 {
-    public static int SelectedMapId { get; set; } = 0;
+    public const int MapCount = 3;
+
+    // Aktualna mapa (0=Easy, 1=Medium, 2=Hard)
+    public static int CurrentMap { get; set; } = 0;
+
+    // Pieniądze przenoszone między mapami
+    public static int PersistentCoins { get; set; } = 120;
+
+    // Wejście do gry – reset całej sesji i start od Easy
+    public static void StartFresh()
+    {
+        CurrentMap = 0;
+        PersistentCoins = 120;
+    }
+
+    // Przejście do następnej mapy; zwraca false jeśli nie ma więcej map
+    public static bool AdvanceMap()
+    {
+        CurrentMap++;
+        return CurrentMap < MapCount;
+    }
+
+    public static bool IsLastMap => CurrentMap >= MapCount - 1;
+
+    // Kompatybilność wsteczna (używane w Hud do etykiety)
+    public static int SelectedMapId => CurrentMap;
 }
